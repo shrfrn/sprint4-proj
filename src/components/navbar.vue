513dbfc19b36f27c113ddfa9896cd3348aa4b7eb
rachel vaikselbom <r45325098@gmail.com>
2021-07-14 15:07:55 +0300

    navbar initial

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,13 +1,22 @@
 <template>
-<section></section>
+  <main class="navbar">
+    <section class="task-action">
+      <router-link to="/boards/123" title="logo">1</router-link>
+      <router-link to="/boards/456" title="boards">2</router-link>
+      <router-link to="/boards/789" title="notification">3</router-link>
+    </section>
+    <section class="user-details">hi rachel</section>
+    <section class="user-action">
+      <router-link to="/boards/102" title="my week">4</router-link>
+      <router-link to="/boards/111" title="my profile">5</router-link>
+      <router-link to="/boards/112" title="loguot">6</router-link>
+    </section>
+  </main>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import app from './app.vue';
+import app from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
-
+import './styles/styles.scss'
 Vue.config.productionTip = false;
 
 new Vue({
