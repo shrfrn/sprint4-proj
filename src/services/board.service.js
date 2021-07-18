d8ad93f0c7849a25e2601f9ce151c47f845a1181
Avior Kahalani <59415129+AviorKahalani@users.noreply.github.com>
2021-07-18 15:14:49 +0300

    more improvments before demo

--- src/services/board.service.js
+++ src/services/board.service.js
@@ -691,7 +691,16 @@ function _getNewGroup() {
             },
         ],
         style: {
-            color: '#5988fa',
+            color: _getRandomColor(),
         },
     };
+}
+
+function _getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
