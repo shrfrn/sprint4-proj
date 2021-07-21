71209f59f55cc83ae098c8f1e4d143087413b6a5
rachel vaikselbom <r45325098@gmail.com>
2021-07-21 11:18:03 +0300

    login from server homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,63 @@
 <template>
-    <div class="home">
-        <div id="nav">
-            <router-link to="/">Home</router-link> <router-link to="/about">About</router-link>
-            <router-link :to="`/board/60f57a5e8ee2f1ab23e50183`">Boards</router-link>
-        </div>
-        <section class="back"></section>
-        <button class="aaa" @click="start">start as guest</button>
-        <button class="aaa" @click="dialogVisible = true">login</button>
-        <el-dialog :visible.sync="dialogVisible" width="30%" :before-close="handleClose">
-            <login-signup @login-signup="handleClose" />
-            <el-button @click="dialogVisible = false">Cancel</el-button>
-        </el-dialog>
-    </div>
+  <div class="home">
+    <section class="header">
+      <div id="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link :to="`/board/60f57a5e8ee2f1ab23e50183`"
+          >Boards</router-link
+        >
+      </div>
+      <section class="back"></section>
+      <button class="aaa" @click="start">start as guest</button>
+      <button class="aaa" @click="dialogVisible = true">login</button>
+      <el-dialog
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose"
+      >
+        <login-signup @login-signup="handleClose" />
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+      </el-dialog>
+    </section>
+    <section class="info">
+      <div class="pro">
+          <div class="icon"><i class="fas fa-tasks"></i></div>
+          <h3>Project Manegment</h3>
+          <p>Project management
+Plan, track, and manage any project from start to finish</p>
+          </div>
+      <div class="icon"><i class="fas fa-code"></i></div>
+      <div class="icon"><i class="fas fa-building"></i></div>
+    </section>
+  </div>
 </template>
 
 <script>
-import loginSignup from './login-signup.vue';
+import loginSignup from "./login-signup.vue";
 export default {
-    components: { loginSignup },
-    name: 'home',
-    data() {
-        return {
-            dialogVisible: false,
-        };
+  components: { loginSignup },
+  name: "home",
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  computed: {
+    initBoard() {
+      return this.$store.getters.boards[0]._id;
     },
-    computed: {
-        initBoard() {
-            return this.$store.getters.boards[0]._id;
-        },
+  },
+  methods: {
+    start() {
+      this.$router.push(`/board/${this.initBoard}`);
     },
-    methods: {
-        start() {
-            this.$router.push(`/board/${this.initBoard}`);
-        },
-        handleClose() {
-            this.dialogVisible = false;
-        },
-        login() {
-            this.$router.push(`login`);
-        },
+    handleClose() {
+      this.dialogVisible = false;
     },
+    login() {
+      this.$router.push(`login`);
+    },
+  },
 };
 </script>
